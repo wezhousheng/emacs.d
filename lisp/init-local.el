1e33efdca7c47322d21d9d7fbae82c91e7e0c3f2
zhousheng <wezhousheng@gmail.com>
2019-02-02 09:54:49 +0800

    add copy-full-path function

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -31,4 +31,9 @@
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
 (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
 
+(defun copy-full-path ()
+  "copy current buffer's full path"
+  (interactive)
+  (kill-new buffer-file-name))
+
 (provide 'init-local)
