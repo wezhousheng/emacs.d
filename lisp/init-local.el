ae3874671111eff76fca20ed7800507e5eacf3d2
zhousheng <wezhousheng@gmail.com>
2019-04-16 16:20:25 +0800

    support markdown preview

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -34,4 +34,18 @@
   (interactive)
   (kill-new buffer-file-name))
 
+;;markdown
+(require-package 'pandoc)
+(custom-set-variables
+ '(markdown-command "/usr/local/bin/pandoc"))
+(require-package 'flymd)
+(defun my-flymd-browser-function (url)
+  (let ((process-environment (browse-url-process-environment)))
+    (apply 'start-process
+           (concat "firefox " url)
+           nil
+           "/usr/bin/open"
+           (list "-a" "firefox" url))))
+(setq flymd-browser-open-function 'my-flymd-browser-function)
+
 (provide 'init-local)
